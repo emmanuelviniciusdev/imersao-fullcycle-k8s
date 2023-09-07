28bef46833c91c1f9b5f643d07e7a7fef7cf87fa
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2023-09-07 08:26:01 -0300

    imersao-fullcycle-k8s

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("[V3] Hello, k8s!"))
+		name := os.Getenv("NAME")
+		wish := os.Getenv("WISH")
+
+		message := "[V4] Olá!! My name is " + name + " and I wish: " + wish
+
+		w.Write([]byte(message))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
