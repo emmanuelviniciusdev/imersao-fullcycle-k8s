0c39ac1464145345edb63d0db8de9315d75ab77e
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2023-09-07 09:57:10 -0300

    imersao-fullcycle-k8s

--- main.go
+++ main.go
@@ -7,14 +7,27 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		name := os.Getenv("NAME")
-		wish := os.Getenv("WISH")
-
-		message := "[V4] Olá!! My name is " + name + " and I wish: " + wish
-
-		w.Write([]byte(message))
-	})
+	http.HandleFunc("/", RootRequestHandler)
+	http.HandleFunc("/most-adorable-countries-on-the-planet", MostAdorableCountriesOnThePlanetRequestHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func MostAdorableCountriesOnThePlanetRequestHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("./countries/countries.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write(data)
+}
+
+func RootRequestHandler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	wish := os.Getenv("WISH")
+
+	message := "[V4] Olá!! My name is " + name + " and I wish: " + wish
+
+	w.Write([]byte(message))
 }
